92a8d57223bc1474d0221ac0d5ff790514dc33d3
Andy Nelson <andy@infinityels.com>
2020-11-28 10:59:15 -0800

    Start setup for page scroll

--- js/page.js
+++ js/page.js
@@ -1,1 +1,6 @@
-console.log('page.js');
+document.addEventListener('click', (event) => {
+    let slideScreen = document.getElementById('slide-screen');
+    if(event.target === slideScreen ) {
+        console.log(event.target);
+    }
+})

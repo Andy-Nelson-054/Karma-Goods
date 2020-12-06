47b19aac270ddcc4f4e958ad9f1f394e5a850248
Andy Nelson <andy@infinityels.com>
2020-12-05 19:35:54 -0800

    add arrow to back page and finish left scroll

--- js/page.js
+++ js/page.js
@@ -1,9 +1,12 @@
-let slideScreen = document.getElementById('slide-screen');
+let slideScreenRight = document.getElementById('slide-screen-right');
+let slideScreenLeft = document.getElementById('slide-screen-left');
 let frontPage = document.getElementById('front-page');
+let backPage = document.getElementById('back-page');
 
 document.addEventListener('click', (event) => {
-    if(event.target === slideScreen ) {
-        console.log(frontPage)
+    
+    //slide screen right
+    if(event.target === slideScreenRight) {
         // window.scrollTo(150, 0)
         this.scroll({
             left: 2000,
@@ -11,4 +14,14 @@ document.addEventListener('click', (event) => {
             behavior: 'smooth'
         })
     }
+
+    //slide screen left
+    if(event.target === slideScreenLeft) {
+        this.scroll({
+            left: 0,
+            top: 0,
+            behavior: 'smooth'
+        })
+    }
+
 });
